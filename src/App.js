d81c6a8efc182130ef0fb656f845e7561907432a
Alvaro Castillo Correa <alvaro.castillo777@gmail.com>
2019-10-09 19:51:18 -0500

    Adding core files

---
+++ src/App.js
@@ -0,0 +1,21 @@
+// App.js
+import React, { Fragment } from 'react';
+import styled from 'styled-components';
+import Header from './components/Header.js';
+import Profile from './components/Profile.js';
+import GlobalStyle from './globalStyle';
+const AppWrapper = styled.div`
+  background-color: #fafafa;
+`;
+function App() {
+  return (
+    <Fragment>
+      <GlobalStyle />
+      <AppWrapper>
+        <Header />
+        <Profile />
+      </AppWrapper>
+    </Fragment>
+  );
+}
+export default App;
